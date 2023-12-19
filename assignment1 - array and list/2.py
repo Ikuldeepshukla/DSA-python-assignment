c8640a7e9f9ca74f801a351ac615b34cf9d1e039
Kuldeep <kuldeepshukla09496@gmail.com>
2023-12-20 02:07:25 +0530

    solution - 02 - remove non integer

--- assignment1 - array and list/2.py
+++ assignment1 - array and list/2.py
@@ -1,1 +1,13 @@
 # Given a list of hetrogeneous elements. Write a python script to remove all non int values from the list.
+
+def removeNonInt(inputList):
+    n = len(inputList)
+    for index in range(n-1, -1, -1):
+        if not isinstance(inputList[index], int):
+            inputList.remove(inputList[index])
+
+    return inputList
+
+dummyInput = [1,2,4,"we", 2, "r", 6, "w"]
+
+print(removeNonInt(dummyInput))
