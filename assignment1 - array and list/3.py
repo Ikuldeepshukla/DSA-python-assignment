c41c32ca0948f3f5b022b7563f8ed09ed7210ed0
Kuldeep <kuldeepshukla09496@gmail.com>
2023-12-20 02:14:44 +0530

    solution - 03 - find average

--- assignment1 - array and list/3.py
+++ assignment1 - array and list/3.py
@@ -1,1 +1,14 @@
 # Write a Python script to calculate average of elements of a list.
+
+def getAverage(nums):
+    count = 0;
+    totalSum = 0;
+    for index in range(len(nums)):
+        if isinstance(nums[index], int):
+            count += 1
+            totalSum += nums[index]
+    return totalSum / count
+
+demoNums = [1,2,3,4,5,6,7]
+
+print(getAverage(demoNums))
