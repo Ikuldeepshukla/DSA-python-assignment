66afa95c0a924a6b75c5f1e3cff86a5343758ace
Kuldeep <kuldeepshukla09496@gmail.com>
2023-12-22 01:46:53 +0530

    solution - fabonacci sequence

--- assignment1 - array and list/5.py
+++ assignment1 - array and list/5.py
@@ -1,1 +1,16 @@
 # Write a Python script to create a list of first N terms of a Fabonacci series.
+
+def getFabonacciSeries(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        sequence = getFabonacciSeries(n - 1)
+        sequence.append(sequence[-1] + sequence[-2])
+        return sequence
+
+    
+print(getFabonacciSeries(5))
