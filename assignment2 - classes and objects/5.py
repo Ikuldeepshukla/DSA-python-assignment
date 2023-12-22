c9c6af915da30ab7a6d8baa4c45f5fcfbe378c97
Kuldeep <kuldeepshukla09496@gmail.com>
2023-12-23 02:10:51 +0530

    solution - classes and object 5.py

--- assignment2 - classes and objects/5.py
+++ assignment2 - classes and objects/5.py
@@ -1,1 +1,27 @@
 # Define a class Team with instance object variable a list of team member names. Provide methods to input member names and display member names.
+
+class Team:
+    def __init__(self, members):
+        self.members = members
+
+    def addMembers(self, newMembers):
+        self.members.extend(newMembers)
+    
+    def displayMembers(self):
+        for member in self.members:
+            print(member, '\n')
+
+    def getMembers(self):
+        return self.members
+    
+# initialize object
+team1 = Team(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+team2 = Team(['H', 'I', 'J', 'K', 'L', 'M'])
+
+team1.displayMembers()
+print("-----------------")
+team2.displayMembers()
+
+print("-----------------")
+team1.addMembers(team2.getMembers())
+team1.displayMembers()
