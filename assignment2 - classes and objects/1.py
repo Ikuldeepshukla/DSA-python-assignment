e352b75e1635dc3bb7f2e0cd756804ba7ac8e70a
Kuldeep <kuldeepshukla09496@gmail.com>
2023-12-23 01:11:17 +0530

    solution - classes and object 1.py

--- assignment2 - classes and objects/1.py
+++ assignment2 - classes and objects/1.py
@@ -1,1 +1,19 @@
 # Define a python class Person with instance object, variables name and age. Set instance object variables in __init__() method. Also define show() method to display name and age.
+
+class Person:
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print('Name:', self.name)
+        print('Age', self.age)
+
+# initialized object 
+p1 = Person('Kuldeep', 28)
+p2 = Person('Ram', 30)
+
+# using object
+p1.show()
+p2.show()
